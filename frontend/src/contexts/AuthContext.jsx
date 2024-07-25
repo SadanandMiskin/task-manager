02dd9856054b0999ed00deb917bc1916271239dd
Sadanand Miskin <miskinsadanand@gmail.com>
2024-07-25 22:08:19 +0530

    Logout: Redirect to homepage

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -44,6 +44,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null);
     localStorage.removeItem('token');
+    window.location.href = '/'
   };
 
   return (
